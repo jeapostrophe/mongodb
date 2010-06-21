3bf71835a25a3a5c56df5e73374a87f6b5ac692d
Jay McCarthy <jay.mccarthy@gmail.com>
2010-06-21 13:28:45 -0600

    Rackety

--- info.rkt
+++ info.rkt
@@ -1,16 +1,18 @@
 #lang setup/infotab
 (define name "MongoDB")
 (define release-notes
-  (list '(ul (li "Fixed bug in BSON binary encoding"))))
+  (list '(ul (li "Fixing quick-start example")
+             (li "Rackety"))))
 (define repositories
   (list "4.x"))
 (define blurb
-  (list "A native Scheme interface to MongoDB"))
+  (list "A native Racket interface to MongoDB"))
 (define scribblings '(("mongodb.scrbl" (multi-page))))
 (define primary-file "main.rkt")
 (define categories '(net io))
 (define compile-omit-paths
   (list "tests.rkt"
+        "quick-start.rkt"
         "basic/tests.rkt"
         "bson/tests.rkt"
         "orm/tests.rkt"
