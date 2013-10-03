a90dd508b2d7db152b957be6c4c7744b68399187
Jay McCarthy <jay.mccarthy@gmail.com>
2013-10-02 19:55:41 -0600

    Forgot to commit this a long time ago

---
+++ info.rkt
@@ -0,0 +1,4 @@
+#lang info
+(define collection 'multi)
+(define deps '("base" "unstable-contract-lib" "web-server-lib"))
+(define build-deps '("eli-tester" "racket-doc" "scribble-lib" "srfi-doc" "web-server-doc"))
