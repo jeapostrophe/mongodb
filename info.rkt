76b8b080be58e2f641b8bda12c6d08ebcff803a8
Jay McCarthy <jay.mccarthy@gmail.com>
2014-07-07 11:50:42 -0400

    update deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "unstable-contract-lib" "web-server-lib"))
+(define deps '("base" "unstable-contract-lib" "web-server-lib" "srfi-lite-lib"))
 (define build-deps '("eli-tester" "racket-doc" "scribble-lib" "srfi-doc" "web-server-doc"))
